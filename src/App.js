5634b1d542913085418b9eda5cd98da9b6a1f6ab
sangeetha309 <159683762+sangeetha309@users.noreply.github.com>
2024-02-11 15:12:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/main/Home";
+
+import "./App.css";
+import "./css/utilities/utilities.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
